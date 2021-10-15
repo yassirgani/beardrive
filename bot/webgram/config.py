b09ba4dc20474680d8534ade30c3858a2355d3bc
AmirHsVIP <75607507+AmirhsVIP@users.noreply.github.com>
2021-10-15 15:38:06 +0000

    Update config.py

--- bot/webgram/config.py
+++ bot/webgram/config.py
@@ -3,14 +3,14 @@ import os
 class Config:
 
     class config:
-        APP_ID = 50807
+        APP_ID = 1540699
 
-        API_HASH = "......"
+        API_HASH = "ca5b7460fe9f242bf5e0e7ed2f4d1001"
         
-        BOT_TOKEN = "......."
+        BOT_TOKEN = "2053666167:AAEwOy6P_4nAoJBOedS-IbMFA8fOcoUSVuE"
                 
         channel = 'UserlandApp'
         
-        ROOT_URI = f"http://dl1.dlgram.ml"
+        ROOT_URI = f"http://rocketdrivebot.herokuapp.com"
 
         ENCODING = "utf8"
