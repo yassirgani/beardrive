b84d5bbf12aff7b7d50a8f1f62f33992eabc4534
yassirgani <92215616+yassirgani@users.noreply.github.com>
2021-10-19 20:08:51 +0330

    Update config.py

--- web/webgram/config.py
+++ web/webgram/config.py
@@ -7,13 +7,13 @@ class Config:
 
         API_HASH = "ca5b7460fe9f242bf5e0e7ed2f4d1001"
         
-        BOT_TOKEN = "2053666167:AAEwOy6P_4nAoJBOedS-IbMFA8fOcoUSVuE"
+        BOT_TOKEN = "2070126389:AAHSq1zn9gpZBeq4L3BzHhSBphVOvBbgKLU"
         
         HOST = "127.0.0.1"
 
         PORT = os.getenv('PORT')
 
-        ROOT_URI = f"http://rocketdrivebot.herokuapp.com"
+        ROOT_URI = f"http://tgdr989.herokuapp.com"
 
 
         ENCODING = "utf8"
